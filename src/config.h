95e3311aa09b2698e6c4cc48bdeb0d2e6b73d61c
Veenya <veramurgia02@gmail.com>
2025-11-28 21:38:21 +0100

    Created configuration file, set pins

---
+++ src/config.h
@@ -0,0 +1,21 @@
+/* Nota: non serve settare i pin dell'LCD
+*/
+
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#define L1_PIN             13 // green 
+#define L2_PIN             12 // green
+#define L3_pin             11 // red
+
+#define BUTTON_PIN         10
+
+#define TEMP_SENSOR_PIN    A0
+#define PIR_PIN            9
+
+#define SONAR_TRIG_PIN     8
+#define SONAR_ECHO_PIN     7
+
+#define MOTOR_PIN          2
+
+#endif
