35d3eb7b9f0b97b5ea10bdf3f93ea5a6fd1a6cc7
Veenya <veramurgia02@gmail.com>
2025-12-01 15:28:18 +0100

    Implementazione del modello Hangar e test hardware di HWPlatform

---
+++ src/model/Hangar.cpp
@@ -0,0 +1,110 @@
+#include "Arduino.h"
+
+#include "Hangar.h"
+#include "config.h"   // per eventuali angoli porta ecc.
+
+// opzionale: definisci qui gli angoli della porta se vuoi
+#ifndef DOOR_OPEN_ANGLE
+#define DOOR_OPEN_ANGLE   90
+#endif
+
+#ifndef DOOR_CLOSED_ANGLE
+#define DOOR_CLOSED_ANGLE 0
+#endif
+
+Hangar::Hangar(HWPlatform* hw)
+  : hw(hw),
+    droneState(DroneState::REST),
+    hangarState(HangarState::NORMAL),
+    droneInside(true),   // da specifica: stato iniziale = dentro
+    doorOpen(false) {
+}
+
+void Hangar::init() {
+  // stato iniziale richiesto:
+  // - porta chiusa
+  // - drone dentro
+  // - hangar normale
+
+  closeDoor();                   // chiude fisicamente la porta
+  droneInside = true;
+  droneState = DroneState::REST;
+  hangarState = HangarState::NORMAL;
+}
+
+/* --------- Stato drone --------- */
+
+void Hangar::setDroneState(DroneState s) {
+  droneState = s;
+}
+
+DroneState Hangar::getDroneState() const {
+  return droneState;
+}
+
+void Hangar::setDroneInside(bool inside) {
+  droneInside = inside;
+}
+
+bool Hangar::isDroneInside() const {
+  return droneInside;
+}
+
+/* --------- Porta hangar --------- */
+
+void Hangar::openDoor() {
+  auto motor = hw->getHangarDoorMotor();
+  if (motor && !doorOpen) {
+    motor->setPosition(DOOR_OPEN_ANGLE);  // es. 90°
+    doorOpen = true;
+  }
+}
+
+void Hangar::closeDoor() {
+  auto motor = hw->getHangarDoorMotor();
+  if (motor && doorOpen) {
+    motor->setPosition(DOOR_CLOSED_ANGLE);  // es. 0°
+    doorOpen = false;
+  }
+}
+
+bool Hangar::isDoorOpen() const {
+  return doorOpen;
+}
+
+/* --------- Letture sensori --------- */
+
+float Hangar::getDistance() {
+  auto sonar = hw->getDDD();
+  if (!sonar) {
+    return NO_OBJ_DETECTED;
+  }
+  return sonar->getDistance();
+}
+
+bool Hangar::isDroneAbove() {
+  auto pir = hw->getDPD();
+  if (!pir) {
+    return false;
+  }
+  // adatta al nome del metodo reale del tuo Pir
+  return pir->isDetected();   // oppure pir->isPresent(), pir->isMotionDetected()...
+}
+
+float Hangar::getTemperature() {
+  auto ts = hw->getTempSensor();
+  if (!ts) {
+    return NAN;
+  }
+  return ts->getTemperature();
+}
+
+/* --------- Stato hangar / allarmi --------- */
+
+void Hangar::setHangarState(HangarState s) {
+  hangarState = s;
+}
+
+HangarState Hangar::getHangarState() const {
+  return hangarState;
+}
---
+++ src/model/Hangar.h
@@ -0,0 +1,54 @@
+#ifndef __HANGAR__
+#define __HANGAR__
+
+#include "States.h"
+#include "HWPlatform.h"
+
+class Hangar {
+
+public:
+  Hangar(HWPlatform* hw);
+
+  void init();
+
+  /* --------- Stato drone --------- */
+
+  void setDroneState(DroneState s);
+  DroneState getDroneState() const;
+
+  void setDroneInside(bool inside);
+  bool isDroneInside() const;
+
+  /* --------- Porta hangar --------- */
+
+  void openDoor();
+  void closeDoor();
+  bool isDoorOpen() const;
+
+  /* --------- Letture sensori --------- */
+
+  // distanza dal sonar (DDD), in metri
+  float getDistance();
+
+  // presenza sopra l’hangar dal PIR (DPD)
+  bool isDroneAbove();
+
+  // temperatura interna (°C)
+  float getTemperature();
+
+  /* --------- Stato hangar / allarmi --------- */
+
+  void setHangarState(HangarState s);
+  HangarState getHangarState() const;
+
+private:
+  HWPlatform* hw;
+
+  DroneState droneState;
+  HangarState hangarState;
+
+  bool droneInside;
+  bool doorOpen;
+};
+
+#endif
