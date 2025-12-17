0aad4a1f0fe2c4e2d253dc23c68e3e8d270655f7
Veenya <veramurgia02@gmail.com>
2025-12-17 20:33:31 +0100

    Copied the java part of the project from old assignment

---
+++ java/readme.md
@@ -0,0 +1,9 @@
+
+## Compilare (powershell)
+`javac -cp "../lib/jssc-2.10.2.jar;../lib/slf4j-api-2.0.17.jar;../lib/slf4j-simple-2.0.17.jar;../lib/native-lib-loader-2.5.0.jar" -d ../../bin iot/*.java`
+
+## Librerie 
+https://www.slf4j.org/download.html
+
+https://mvnrepository.com/artifact/io.github.java-native/jssc/2.9.6?referrer=grok.com
+https://repo1.maven.org/maven2/io/github/java-native/jssc/2.10.2/
---
+++ java/src/iot/drone_carrier/DashboardLauncher.java
@@ -0,0 +1,27 @@
+package iot.drone_carrier;
+
+import javax.swing.SwingUtilities;
+
+class DashboardLauncher   {
+
+	static DashboardView view = null;
+	static LogView log = null;
+
+	public static void main(String[] args) throws Exception {	
+		/*
+		if (args.length != 1){
+			System.err.println("Args: <serial port>");
+			System.exit(1);
+		}*/
+
+		view = new DashboardView();
+		log = new LogView();
+		
+		String portName = "COM5";
+		DashboardController contr = new DashboardController(portName,view,log);
+		view.registerController(contr);
+		
+		view.display();
+		log.display();
+	}
+}
