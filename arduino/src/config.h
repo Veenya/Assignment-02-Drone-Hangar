f6b54c62cb8110bcc678bbc219db51886beeba15
Veenya <veramurgia02@gmail.com>
2025-12-17 20:21:26 +0100

    Restructuring

---
+++ arduino/src/config.h
@@ -0,0 +1,20 @@
+// Sotto sta il mio
+#ifndef __CONFIG__
+#define __CONFIG__
+
+#define L1_PIN             13 // green 
+#define L2_PIN             12 // green
+#define L3_PIN             11 // red
+
+#define BUTTON_PIN         10
+
+#define TEMP_PIN           A0
+#define PIR_PIN            9
+#define MAX_TIME_US        30000L      
+#define LCD_ADDR           0x27
+#define SONAR_TRIG_PIN     8
+#define SONAR_ECHO_PIN     7
+
+#define SERVO_PIN          2
+
+#endif
---
+++ arduino/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "kernel/MsgService.h"
+#include "kernel/Logger.h"
+
+void setup() {
+    MsgService.init();
+    delay(2000);
+    MsgService.sendMsg("lo:arduino ready");
+}
+
+
+void loop() {
+  Logger.log(F("PING"));             // manda "lo:PING" ogni secondo
+  delay(1000);
+}
