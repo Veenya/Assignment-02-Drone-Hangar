01fcfe397f1e23070d3d5c2f5fcbf26b26d79cb4
Veenya <veramurgia02@gmail.com>
2025-12-20 09:49:02 +0100

    Modificato il contenuto della cartella Button, ho rifatto come il prof

--- arduino/src/devices/button/Button.h
+++ arduino/src/devices/button/Button.h
@@ -3,7 +3,16 @@
 
 class Button {
     public:
+        Button();
         virtual bool isPressed() = 0;
+        virtual bool isClicked() = 0;
+
+        virtual void sync(); // TODO: controlla dove
+        long getLastSynchTime(); // TODO: rivedere
+    protected:
+        void updateSyncTime(long time); // TODO
+    private:
+        long lastTimeSync; // TODO
 };
 
 #endif
--- arduino/src/devices/button/ButtonImpl.h
+++ arduino/src/devices/button/ButtonImpl.h
@@ -7,8 +7,12 @@ class ButtonImpl: public Button {
     public:
         ButtonImpl(int pin);
         bool isPressed();
+        bool isClicked();
+        void sync();
     protected:
         int pin;
+        bool pressed;
+        bool clicked;
 };
 
 #endif
