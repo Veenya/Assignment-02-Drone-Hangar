897b5cfd0f42767a537d1eaf50f0f13ee41b73f5
Veenya <veramurgia02@gmail.com>
2025-12-21 23:15:43 +0100

    Login view now bigger

--- java/src/iot/drone_carrier/LogView.java
+++ java/src/iot/drone_carrier/LogView.java
@@ -1,5 +1,8 @@
 package iot.drone_carrier;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.util.Date;
 
 import javax.swing.*;
@@ -9,24 +12,44 @@ class LogView extends JFrame  {
 	private JTextArea log;
 
 	public LogView(){
-		super("Log ");
-		setSize(600,600);
+		super("Log");
+
+		// Finestra più grande
+		setSize(900, 700);
 		this.setResizable(false);
+
 		JPanel mainPanel = new JPanel();
-		log = new JTextArea(30,40);
+
+		// Area di log più grande
+		log = new JTextArea(35, 80);
+
+		// Importante: NON disabilitarla (setEnabled(false)) altrimenti diventa grigia.
+		// Basta renderla non editabile.
 		log.setEditable(false);
-		JScrollPane scrol = new JScrollPane(log);
-		mainPanel.add(scrol);
+
+		// Sfondo bianco + testo nero
+		log.setBackground(Color.WHITE);
+		log.setForeground(Color.BLACK);
+
+		// Testo più grande (monospace comodo per log)
+		log.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 18));
+
+		// Scroll pane più grande
+		JScrollPane scroll = new JScrollPane(log);
+		scroll.setPreferredSize(new Dimension(860, 650));
+
+		mainPanel.add(scroll);
 		this.getContentPane().add(mainPanel);
 	}
-	
+
 	public void log(String msg){
 		SwingUtilities.invokeLater(() -> {
 			String date = new Date().toString();
-			log.append("["+date+"] "+ msg +"\n");
+			log.append("[" + date + "] " + msg + "\n");
+			log.setCaretPosition(log.getDocument().getLength()); // autoscroll in fondo
 		});
 	}
-	
+
 	public void display() {
 		SwingUtilities.invokeLater(() -> {
 			this.setVisible(true);
